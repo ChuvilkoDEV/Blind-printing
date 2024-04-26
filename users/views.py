fe3095c4aa6027341636a1604e1fb2dc536bdd51
IlyaChuvilko <89748002+ChuvilkoDEV@users.noreply.github.com>
2024-04-26 20:21:54 +0300

    Добавлена страница регистрации

--- users/views.py
+++ users/views.py
@@ -1,15 +1,16 @@
 from django.http import HttpResponse
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth import logout
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 
 from blind.utils import DataMixin
+from users.forms import LoginUserForm, RegisterUserForm
 
 
 class LoginUser(DataMixin, LoginView):
-    form_class = AuthenticationForm
+    form_class = LoginUserForm
     template_name = 'login.html'
     next_page = reverse_lazy('index')
 
@@ -18,10 +19,23 @@ class LoginUser(DataMixin, LoginView):
         return self.get_mixin_context(context, title='Авторизация')
 
 
+# class LogoutUser(DataMixin, LogoutView):
+#     next_page = reverse_lazy('index')
+
+
 def logout_user(request):
     logout(request)
-    return redirect('users:login')
+    return redirect('index')
 
 
 def register(request):
-    return HttpResponse("Страница регистрации")
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password1'])
+            user.save()
+            return redirect('users:login')
+    else:
+        form = RegisterUserForm()
+    return render(request, 'register.html', {'form': form})
