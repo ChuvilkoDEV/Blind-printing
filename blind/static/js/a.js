274754105b049b1c23519611f6f2825c7a382198
ChuvilkoDEV <ilyasutton@gmail.com>
2024-05-04 20:30:44 +0300

    Модифицировал поле ввода

--- blind/static/js/a.js
+++ blind/static/js/a.js
@@ -4,18 +4,23 @@ templateField = document.getElementById("templateField");
 // Слушаем событие ввода символа
 document.addEventListener("keydown", function (event) {
     let symbol = event.key;
-    if (symbol.length === 1 && symbol.match(/[a-zA-Z0-9]/)) {
+    if (symbol.length === 1 && symbol.match(/[a-zA-Zsа-яА-Я0-9,. ]/)) {
         event.preventDefault();
-        if (symbol === templateField.textContent[0]) {
+        if (symbol === templateField.value[0]) {
             inputField.value += symbol;
-            templateField.textContent = templateField.textContent.slice(1);
+            templateField.value = templateField.value.slice(1);
         } else {
             inputField.classList.add('shake');
             setTimeout(function () {
                 inputField.classList.remove('shake');
             }, 600);
         }
-    } else if (event.key === 'Enter') {
+    } else if (symbol === 'Enter') {
         event.preventDefault();
     }
 });
+
+const dataToSend = {
+  key1: 'value1',
+  key2: 'value2'
+};
