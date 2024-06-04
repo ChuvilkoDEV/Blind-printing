cba19e0a6bc3c424d24fed2cd9a7341c34521d82
ChuvilkoDEV <ilyasutton@gmail.com>
2024-06-04 13:32:29 +0300

    Добавлена валидация введенных символов на стороне клиента

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,64 +1,95 @@
-import React, { useEffect, useState } from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 
-export default function Main() {
-  const [randomTextData, setRandomTextData] = useState([]);
-  const [randomText, setRandomText] = useState([]);
-  const [inputText, setInputText] = useState('');
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      randomTextData: [],
+      randomText: [],
+      inputText: '',
+      keyPressCount: 0,  // Добавляем новый state для счетчика нажатий клавиш
+    };
+  }
 
-  useEffect(() => {
-    const fetchRandomText = async () => {
-      try {
-        const response = await axios.get('http://localhost:8000/api/v1/random-text/');
-        if (response.data && response.data.text) {
-          setRandomTextData(response.data);
-          setRandomText(response.data.text.split(''));  // Преобразуем строку в массив символов
-        } else {
-          console.error('Response data is not in the expected format:', response.data);
-        }
-      } catch (error) {
-        console.error('Error fetching random text:', error);
+  componentDidMount() {
+    this.fetchRandomText();
+
+    document.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  fetchRandomText = async () => {
+    try {
+      const response = await axios.get('http://localhost:8000/api/v1/random-text/');
+      if (response.data && response.data.text) {
+        this.setState({
+          randomTextData: response.data,
+          randomText: response.data.text.split(''),  // Преобразуем строку в массив символов
+        });
+      } else {
+        console.error('Response data is not in the expected format:', response.data);
       }
-    };
+    } catch (error) {
+      console.error('Error fetching random text:', error);
+    }
+  };
 
-    fetchRandomText();
+  handleKeyDown = (event) => {
+    const key = event.key;
+    var regex = /^[ a-zA-Z0-9а-яА-Я.,?!:;'"()\-]+$/;
+    
+    if (key.length == 1 && regex.test(key)) {
+      this.setState((prevState) => ({
+        inputText: prevState.inputText + key,
+        keyPressCount: prevState.keyPressCount + 1,  // Увеличиваем счетчик
+      }));
+    } else if (key === 'Backspace' && this.state.inputText.length > 0) {
+      console.log(1)
+      this.setState((prevState) => ({
+        inputText: prevState.inputText.slice(0, -1),
+        keyPressCount: prevState.keyPressCount - 1,  // Увеличиваем счетчик
+      }));
+    }
+  };
 
-    const handleKeyDown = (event) => {
-      console.log(event.key);
-      setInputText((prevText) => prevText + event.key);
-      console.log(inputText);
+  render() {
+    return (
+      <div>
+        <Game
+          randomText={this.state.randomText}
+          inputText={this.state.inputText}
+          keyPressCount={this.state.keyPressCount}  // Передаем счетчик в компонент Game
+        />
+      </div>
+    );
+  }
+}
 
-    };
-
-    document.addEventListener('keydown', handleKeyDown);
-
-    return () => {
-      document.removeEventListener('keydown', handleKeyDown);
-    };
-  }, []);
-
-  const InputField = () => {
+class Game extends Component {
+  render() {
     return (
       <div>
         <p>
-          {randomText.map((symbol, index) => (
+          {this.props.randomText.map((symbol, index) => (
             <span
               key={index}
               style={{
-                color: inputText[index] === symbol ? 'green' : 'red',
+                color: this.props.inputText[index] === symbol ? 'green' : 'red',
               }}
             >
               {symbol}
             </span>
           ))}
         </p>
+        <p>inputedText: {this.props.inputText}</p>
+        <p>Key Press Count: {this.props.keyPressCount}</p>  {/* Отображаем счетчик */}
       </div>
     );
-  };
-
-  return (
-    <div>
-      <InputField />
-    </div>
-  );
+  }
 }
+
+export default Main;
--- frontend/src/components/Theory.js
+++ frontend/src/components/Theory.js
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom'
 
 function Card(article) {
   return <><Link to={article.article.slug} className='card link-no-underline'>
-  <img src={article.article.image} class="card-img-top"></img>
-  <div class="card-body">
-    <h5 class="card-title">{article.article.title}</h5>
-    <p class="card-text">{article.article.content}...</p>
-    <a href="#" class="btn btn-primary">Читать</a>
+  <img src={article.article.image} className="card-img-top"></img>
+  <div className="card-body">
+    <h5 className="card-title">{article.article.title}</h5>
+    <p className="card-text">{article.article.content}...</p>
+    <a href="#" className="btn btn-primary">Читать</a>
   </div>
 </Link>
 </>
