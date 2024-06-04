3975fa24da365ed9b1f9e1076258e62d95214ba0
ChuvilkoDEV <ilyasutton@gmail.com>
2024-06-04 20:38:23 +0300

    Исправлено отображение подсветки для слов

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 import '../css/Main.css'; // Import the created CSS file
 
@@ -44,28 +44,40 @@ class Main extends Component {
     const regex = /^[a-zA-Z0-9а-яА-Я.,?!:;'"()\-]+$/;
 
     if (key === ' ') {
-      if (this.state.inputText[-1].length !== 0) {
+      if (this.state.inputText[this.state.inputText.length - 1].length !== 0) {
         this.setState((prevState) => ({
-          inputText: prevState.inputText.push(''),
+          inputText: [...prevState.inputText, ''],
           keyPressCount: prevState.keyPressCount + 1,
           loggedEvents: [...prevState.loggedEvents, event],
         }));
       }
       return;
     }
-    
-    if (key.length === 1 && regex.test(key) && this.state.inputText.length < this.state.randomText.join(' ').length) {
-      this.setState((prevState) => ({
-        inputText: prevState.inputText[-1] + key,
-        keyPressCount: prevState.keyPressCount + 1,
-        loggedEvents: [...prevState.loggedEvents, event],
-      }));
-    } else if (key === 'Backspace' && this.state.inputText.length > 0) {
-      this.setState((prevState) => ({
-        inputText: prevState.inputText.slice(0, -1),
-        keyPressCount: prevState.keyPressCount - 1,
-        loggedEvents: [...prevState.loggedEvents, event],
-      }));
+
+    if (key.length === 1 && regex.test(key)) {
+      this.setState((prevState) => {
+        const inputText = [...prevState.inputText];
+        inputText[inputText.length - 1] += key;
+        return {
+          inputText,
+          keyPressCount: prevState.keyPressCount + 1,
+          loggedEvents: [...prevState.loggedEvents, event],
+        };
+      });
+    } else if (key === 'Backspace') {
+      this.setState((prevState) => {
+        const inputText = [...prevState.inputText];
+        if (inputText[inputText.length - 1].length === 0 && inputText.length > 1) {
+          inputText.pop();
+        } else {
+          inputText[inputText.length - 1] = inputText[inputText.length - 1].slice(0, -1);
+        }
+        return {
+          inputText,
+          keyPressCount: prevState.keyPressCount - 1,
+          loggedEvents: [...prevState.loggedEvents, event],
+        };
+      });
     }
   };
 
@@ -84,7 +96,10 @@ class Main extends Component {
 
 class Game extends Component {
   getColor = (symbol, wordIndex, symbolIndex) => {
-    if (wordIndex >= this.props.inputText.length || symbolIndex >= this.props.inputText[wordIndex].length) {
+    if (
+      wordIndex >= this.props.inputText.length ||
+      symbolIndex >= this.props.inputText[wordIndex].length
+    ) {
       return 'gray';
     }
     return this.props.inputText[wordIndex][symbolIndex] === symbol ? 'green' : 'red';
@@ -93,19 +108,21 @@ class Game extends Component {
   render() {
     return (
       <div>
-        <p className="main-text"><b>
-          {this.props.randomText.map((word, wordIndex) => (
-            <span key={wordIndex}>
-              {word.split('').map((symbol, symbolIndex) => (
-                <span key={wordIndex + '-' + symbolIndex} style={{color: this.getColor(symbol, symbolIndex)}}>
-                  {symbol}
-                </span>
-              ))}
-              {' '}
-            </span>
-          ))}
-        </b></p>
-        <p className="main-text">inputedText: {this.props.inputText}</p>
+        <p className="main-text">
+          <b>
+            {this.props.randomText.map((word, wordIndex) => (
+              <span key={wordIndex}>
+                {word.split('').map((symbol, symbolIndex) => (
+                  <span key={wordIndex + '-' + symbolIndex} style={{ color: this.getColor(symbol, wordIndex, symbolIndex) }}>
+                    {symbol}
+                  </span>
+                ))}
+                {' '}
+              </span>
+            ))}
+          </b>
+        </p>
+        <p className="main-text">inputedText: {this.props.inputText.join(' ')}</p>
         <p>Key Press Count: {this.props.keyPressCount}</p>
       </div>
     );
